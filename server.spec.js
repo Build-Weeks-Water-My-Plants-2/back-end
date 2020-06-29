17b46bc3001649c0a238009798dbc7634f01d47f
Yarriba Castro <yarribacastro@Yarribas-MacBook-Pro.local>
2020-06-28 21:36:14 -0500

    testing completed

--- server.spec.js
+++ server.spec.js
@@ -1,8 +1,8 @@
-const request = require('supertest');
-const server = require('./server.js');
-const db = require('./data/db-config.js');
+const request = require("supertest");
+const server = require("./server.js");
+const db = require("./data/db-config.js");
 
-describe('server.js', () => {
+describe("server.js", () => {
   let userInfo = null;
   let registerRes = null;
   let token = null;
@@ -11,219 +11,264 @@ describe('server.js', () => {
   beforeAll(async () => {
     userInfo = {
       username: "Nick",
-      password: "123456"
+      password: "123456",
     };
-    registerRes = await request(server).post('/auth/register').send(userInfo);
+    registerRes = await request(server).post("/auth/register").send(userInfo);
     token = registerRes.body.token;
     userId = registerRes.body.data.id;
   });
 
   afterAll(async () => {
-    await db('user').truncate();
-    await db('plant').truncate();
+    await db("user").truncate();
+    await db("plant").truncate();
   });
 
-  describe('authentication', () => {
-
-    describe('POST /auth/register', () => {
-
-      it('should return a Created status code', async () => {
-        const response = await request(server)
-          .post('/auth/register')
-          .send({
-            username: 'registerTest',
-            password: '123456'
-          });
+  describe("authentication", () => {
+    describe("POST /auth/register", () => {
+      it("should return a Created status code", async () => {
+        const response = await request(server).post("/auth/register").send({
+          username: "registerTest",
+          password: "123456",
+        });
         expect(response.status).toEqual(201);
       });
 
-      it('should return a user object', async () => {
-        const response = await request(server)
-          .post('/auth/register')
-          .send({
-            username: 'registerTest2',
-            password: '123456'
-          });
+      it("should return a user object", async () => {
+        const response = await request(server).post("/auth/register").send({
+          username: "registerTest2",
+          password: "123456",
+        });
         expect(response.body.data.username).toEqual("registerTest2");
       });
 
-      it('should return a JWT', async () => {
-        const response = await request(server)
-          .post('/auth/register')
-          .send({
-            username: 'registerTest3',
-            password: '123456'
-          });
-        expect(typeof(response.body.token)).toEqual("string");
+      it("should return a JWT", async () => {
+        const response = await request(server).post("/auth/register").send({
+          username: "registerTest3",
+          password: "123456",
+        });
+        expect(typeof response.body.token).toEqual("string");
       });
 
-      it('should return a 422 for blank username', async () => {
-        const response = await request(server)
-          .post('/auth/register')
-          .send({
-            username: '',
-            password: '123456'
-          });
+      it("should return a 422 for blank username", async () => {
+        const response = await request(server).post("/auth/register").send({
+          username: "",
+          password: "123456",
+        });
         expect(response.status).toEqual(422);
       });
 
-      it('should return a 400 for missing username', async () => {
-        const response = await request(server)
-          .post('/auth/register')
-          .send({
-            password: '123456'
-          });
+      it("should return a 400 for missing username", async () => {
+        const response = await request(server).post("/auth/register").send({
+          password: "123456",
+        });
         expect(response.status).toEqual(400);
       });
-
     });
 
-    describe('POST /auth/login', () => {
-      
-      it('should return a 200 status code', async () => {
-        const response = await request(server)
-          .post('/auth/login')
-          .send({
-            username: 'Nick',
-            password: '123456'
-          });
+    describe("POST /auth/login", () => {
+      it("should return a 200 status code", async () => {
+        const response = await request(server).post("/auth/login").send({
+          username: "Nick",
+          password: "123456",
+        });
         expect(response.status).toEqual(200);
       });
 
-      it('should return a JWT', async () => {
-        const response = await request(server)
-          .post('/auth/login')
-          .send({
-            username: 'Nick',
-            password: '123456'
-          });
-        expect(typeof(response.body.token)).toEqual("string");
+      it("should return a JWT", async () => {
+        const response = await request(server).post("/auth/login").send({
+          username: "Nick",
+          password: "123456",
+        });
+        expect(typeof response.body.token).toEqual("string");
       });
 
-      it('should return a 401 for the wrong password', async () => {
-        const response = await request(server)
-          .post('/auth/login')
-          .send({
-            username: 'Nick',
-            password: 'Wrong password'
-          });
+      it("should return a 401 for the wrong password", async () => {
+        const response = await request(server).post("/auth/login").send({
+          username: "Nick",
+          password: "Wrong password",
+        });
         expect(response.status).toEqual(401);
       });
 
-      it('should return a 422 for blank username', async () => {
-        const response = await request(server)
-          .post('/auth/login')
-          .send({
-            username: '',
-            password: '123456'
-          });
+      it("should return a 422 for blank username", async () => {
+        const response = await request(server).post("/auth/login").send({
+          username: "",
+          password: "123456",
+        });
         expect(response.status).toEqual(422);
       });
 
-      it('should return a 400 for missing username', async () => {
-        const response = await request(server)
-          .post('/auth/login')
-          .send({
-            password: '123456'
-          });
+      it("should return a 400 for missing username", async () => {
+        const response = await request(server).post("/auth/login").send({
+          password: "123456",
+        });
         expect(response.status).toEqual(400);
       });
-
     });
-
   });
 
-  describe('users', () => {
-
-    describe('GET /users/:id', () => {
-
-      it('should return an OK status code', async () => {
+  describe("users", () => {
+    describe("GET /users/:id", () => {
+      it("should return an OK status code", async () => {
         const expectedStatusCode = 200;
-        const response = await request(server).get('/users/' + userId).set({
-          Authorization: token
-        });
+        const response = await request(server)
+          .get("/users/" + userId)
+          .set({
+            Authorization: token,
+          });
         expect(response.status).toEqual(expectedStatusCode);
       });
 
-      it('should return the username and user id', async () => {
+      it("should return the username and user id", async () => {
         const expectedUsername = "Nick";
         const expectedId = 1;
-        const response = await request(server).get('/users/' + userId).set({
-          Authorization: token
-        });
+        const response = await request(server)
+          .get("/users/" + userId)
+          .set({
+            Authorization: token,
+          });
         expect(response.body.username).toEqual(expectedUsername);
         expect(response.body.id).toEqual(expectedId);
       });
 
-      it('should return 401 if no Authorization', async () => {
+      it("should return 401 if no Authorization", async () => {
         const expectedStatusCode = 401;
         const expectedMessage = "Missing authorization header";
-        const response = await request(server).get('/users/' + userId);
+        const response = await request(server).get("/users/" + userId);
         expect(response.status).toEqual(expectedStatusCode);
         expect(response.body.message).toEqual(expectedMessage);
       });
 
-      it('should return 401 if bad Authorization', async () => {
+      it("should return 401 if bad Authorization", async () => {
         const expectedStatusCode = 401;
         const expectedMessage = "Invalid Credentials";
-        const response = await request(server).get('/users/' + userId).set({
-          Authorization: "wrong token"
-        });
+        const response = await request(server)
+          .get("/users/" + userId)
+          .set({
+            Authorization: "wrong token",
+          });
         expect(response.status).toEqual(expectedStatusCode);
         expect(response.body.message).toEqual(expectedMessage);
       });
-
     });
 
-    describe('PUT /users/:id', () => {
+    describe("PUT /users/:id", () => {
       let userInfo = {
         username: "NickForPut",
-        password: "123456"
+        password: "123456",
       };
       let registerRes = null;
       let token = null;
       let putUserId = null;
 
       beforeAll(async () => {
-        registerRes = await request(server).post('/auth/register').send(userInfo);
+        registerRes = await request(server)
+          .post("/auth/register")
+          .send(userInfo);
         token = registerRes.body.token;
         putUserId = registerRes.body.data.id;
       });
 
-      it('should return an OK status code', async () => {
-        const response = await request(server).put('/users/' + putUserId).set({
-          Authorization: token
-        }).send({
-          phone_number: "555-5555"
-        });
+      it("should return an OK status code", async () => {
+        const response = await request(server)
+          .put("/users/" + putUserId)
+          .set({
+            Authorization: token,
+          })
+          .send({
+            phone_number: "555-5555",
+          });
         expect(response.status).toEqual(200);
       });
 
-      it('should return return the updated user', async () => {
-        const response = await request(server).put('/users/' + putUserId).set({
-          Authorization: token
-        }).send({
-          phone_number: "123-4567"
-        });
+      it("should return return the updated user", async () => {
+        const response = await request(server)
+          .put("/users/" + putUserId)
+          .set({
+            Authorization: token,
+          })
+          .send({
+            phone_number: "123-4567",
+          });
         expect(response.body).toMatchObject({
           username: "NickForPut",
-          phone_number: "123-4567"
+          phone_number: "123-4567",
         });
       });
 
-      it('response should match the user in the db', async () => {
-        const response = await request(server).put('/users/' + putUserId).set({
-          Authorization: token
-        }).send({
-          phone_number: "987-6543"
-        });
-        const dbUser = await db('user').where({id: putUserId}).first();
+      it("response should match the user in the db", async () => {
+        const response = await request(server)
+          .put("/users/" + putUserId)
+          .set({
+            Authorization: token,
+          })
+          .send({
+            phone_number: "987-6543",
+          });
+        const dbUser = await db("user").where({ id: putUserId }).first();
         expect(response.body).toMatchObject(dbUser);
       });
     });
-
   });
 
-  describe('plants', () => {
+  describe("plants", () => {
+    let plantResponse = null;
+    let plantId = null;
+
+    beforeAll(async () => {
+      plantResponse = await request(server)
+        .post("/plants")
+        .set({
+          Authorization: token,
+        })
+        .send({
+          nickname: "planty",
+          species: "something",
+        });
+      plantId = plantResponse.body.id;
+    });
+    //Test GET Plants
+    describe("GET /plants/:id", () => {
+      it("Should return OK status code 200", async () => {
+        console.log(plantId);
+        const expectedStatusCode = 200;
+        const response = await request(server)
+          .get("/plants/" + plantId)
+          .set({
+            Authorization: token,
+          });
+        expect(response.status).toEqual(expectedStatusCode);
+      });
+    });
+
+    //PUT Update plants
+    describe("PUT /plants/:id", () => {
+      it("Should Update plants entry, code 201", async () => {
+        const expectedStatusCode = 201;
+        const response = await request(server)
+          .put("/plants/" + plantId)
+          .set({
+            Authorization: token,
+          })
+          .send({
+            nickname: "Sleepy Tea",
+            species: "camomile",
+          });
+        expect(response.status).toEqual(expectedStatusCode);
+      });
+    });
+
+    //DELETE plants
+    describe("DELETE /plants/:id", () => {
+      it("Should Delete plant entry, code 204", async () => {
+        const expectedStatusCode = 204;
+        const response = await request(server)
+          .delete("/plants/" + plantId)
+          .set({
+            Authorization: token,
+          });
+        expect(response.status).toEqual(expectedStatusCode);
+      });
+    });
   });
 });
